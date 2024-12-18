f85a2b88213c2433ae95a75c751df38a298ee036
SARDORALOYEV <aloyevsardor30@gmail.com>
2024-12-18 11:54:45 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+let first = prompt("Enter your name");
+let surname = prompt("Enter your surname");
+let age = +prompt("Enter your age");
+let like = prompt("Enter your favourite science");
+let locat = prompt("Enter your location");
+console.log("Your name:" , first);
+console.log("Your surname:" , surname);
+console.log("Your age:" , age);
+console.log("Your favourite science:" , like);
+console.log("Your location:" , locat);
